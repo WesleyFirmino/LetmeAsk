eba1ce5749dceacdd729c370686bd27d51f77920
Wesley Firmino <wesleydasilva_firmino@live.com>
2021-09-17 12:29:21 -0300

    Create pages and authentication google user

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,40 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../App";
+
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import { Button } from "../components/Button";
+
+import "../styles/auth.scss";
+
+export function NewRoom() {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
+        <strong>Crie salas de Q&A ao vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Logo da aplicação" />
+          <h1>{user?.name}</h1>
+          <h2>Nome da sala</h2>
+          <form>
+            <input type="text" placeholder="Digite o código da sala" />
+            <Button type="submit">Entrar na sala</Button>
+          </form>
+          <p>
+            Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
