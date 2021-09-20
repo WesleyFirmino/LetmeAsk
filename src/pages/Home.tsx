dbeb17aa7d49178b9b0e68c767a1676603c1b6f2
Wesley Firmino <wesleydasilva_firmino@live.com>
2021-09-20 11:15:32 -0300

    Hooks Authentication

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,12 +5,11 @@ import googleIconImg from "../assets/images/google-icon.svg";
 
 import "../styles/auth.scss";
 import { Button } from "../components/Button";
-import { useContext } from "react";
-import { AuthContext } from "../App";
+import { useAuth } from "../hooks/userAuth";
 
 export function Home() {
   const history = useHistory();
-  const { user, signInWithGoogle } = useContext(AuthContext);
+  const { user, signInWithGoogle } = useAuth();
 
   async function handleCreateRoom() {
     if (!user) {
